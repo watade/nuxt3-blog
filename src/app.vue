b899305cfa3e9bbf929dc97308035090e9f9dfb4
kitade <dekitawatiked@gmail.com>
2023-09-15 01:39:50 +0900

    add padding to bottom of the page

--- src/app.vue
+++ src/app.vue
@@ -9,7 +9,7 @@ useHead({
 <template>
   <div class="px-8 sm:px-20 lg:px-30 xl:px-40">
     <MyHeader />
-    <div class="max-w-3xl lg:max-w-4xl xl:max-w-5xl 2xl:max-w-6xl mx-auto px-3">
+    <div class="max-w-3xl lg:max-w-4xl xl:max-w-5xl 2xl:max-w-6xl mx-auto px-3 pb-8">
       <NuxtPage />
     </div>
   </div>
