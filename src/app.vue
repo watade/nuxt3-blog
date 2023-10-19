ed15ac60de6a30817d69e16ad2b95bc9bef73556
kitade <dekitawatiked@gmail.com>
2023-10-19 21:27:20 +0900

    limit overscroll

--- src/app.vue
+++ src/app.vue
@@ -7,7 +7,7 @@ useHead({
 </script>
 
 <template>
-  <div class="px-8 sm:px-20 lg:px-30 xl:px-40">
+  <div class="px-8 sm:px-20 lg:px-30 xl:px-40 overscroll-none">
     <MyHeader />
     <div class="max-w-3xl lg:max-w-4xl xl:max-w-5xl 2xl:max-w-6xl mx-auto px-3 pb-8">
       <NuxtPage />
