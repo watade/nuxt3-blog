7f1a0b7ec791bb0f79d59fd22de9548f862ecc66
kitade <wataru.kitade@gmail.com>
2024-12-11 01:21:34 +0900

    avoid date hydration error

--- src/plugins/date-format.ts
+++ src/plugins/date-format.ts
@@ -1,11 +1,11 @@
 import dayjs from 'dayjs';
-import 'dayjs/locale/ja';
+import ja from 'dayjs/locale/ja';
 
 export default defineNuxtPlugin(() => {
   return {
     provide: {
       formatDate(datetime: string): string {
-        return dayjs(datetime).locale('ja').format('YYYY年MM月DD日');
+        return dayjs(datetime).locale(ja).format('YYYY年MM月DD日');
       },
     },
   };
